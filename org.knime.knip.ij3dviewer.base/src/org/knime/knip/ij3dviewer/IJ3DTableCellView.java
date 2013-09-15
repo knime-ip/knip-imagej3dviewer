446e5346c705dd3112b1788b10c6fe9a8c459326
Gabriel Einsdorf <gabriel.einsdorf@uni.kn>
2013-09-15 21:20:14 +0200

    commit to cherrypick from

--- org.knime.knip.ij3dviewer.base/src/org/knime/knip/ij3dviewer/IJ3DTableCellView.java
+++ org.knime.knip.ij3dviewer.base/src/org/knime/knip/ij3dviewer/IJ3DTableCellView.java
@@ -79,8 +79,6 @@ public class IJ3DTableCellView<T extends RealType<T>> implements TableCellView {
 		rootPanel.add(ij3dbar, BorderLayout.NORTH);
 		rootPanel.add(universe.getCanvas(0), BorderLayout.CENTER);
 
-		// rootPanel.add(panel4D, BorderLayout.SOUTH);
-
 		return rootPanel;
 	}
 
@@ -104,19 +102,19 @@ public class IJ3DTableCellView<T extends RealType<T>> implements TableCellView {
 			logger.warn("Error: only immages with up to 5 Dimensions are supported by the 3D viewer");
 			return;
 		}
-		ImgToIJ imgToIJ = new ImgToIJ(imgPlus.numDimensions());
+		ImgToIJ imgToIJ = new ImgToIJ();
 
-		if (!imgToIJ.validateMapping(imgPlus)) {
-			logger.warn("Warning: couldn't match dimensions of input picture, using Standard dimension settings as best guess");
-			HashMap<AxisType, Integer> newMapping = new HashMap<AxisType, Integer>();
-
-			for (int d = 0; d < imgPlus.numDimensions(); d++) {
-				newMapping.put(imgPlus.axis(0).type(), d);
-			}
-
-			imgToIJ.setMapping(newMapping);
-		}
-		
+//		if (!imgToIJ.validateMapping(imgPlus)) {
+//			logger.warn("Warning: couldn't match dimensions of input picture, using Standard dimension settings as best guess");
+//			HashMap<AxisType, Integer> newMapping = new HashMap<AxisType, Integer>();
+//
+//			for (int d = 0; d < imgPlus.numDimensions(); d++) {
+//				newMapping.put(imgPlus.axis(0).type(), d);
+//			}
+//
+//			imgToIJ.setMapping(newMapping);
+//		}
+//		
 		ijImagePlus = Operations.compute(imgToIJ, imgPlus);
 
 		new StackConverter(ijImagePlus).convertToGray8();
