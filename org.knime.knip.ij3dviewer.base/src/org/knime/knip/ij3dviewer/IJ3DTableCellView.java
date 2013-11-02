65bca55679e3ed57e77ae73891c9570d4df3c660
Gabriel Einsdorf <gabriel.einsdorf@uni.kn>
2013-11-02 17:33:58 +0100

    copyright notice added and cleanups

--- org.knime.knip.ij3dviewer.base/src/org/knime/knip/ij3dviewer/IJ3DTableCellView.java
+++ org.knime.knip.ij3dviewer.base/src/org/knime/knip/ij3dviewer/IJ3DTableCellView.java
@@ -1,3 +1,51 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 - 2013
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * --------------------------------------------------------------------- *
+ *
+ */
 package org.knime.knip.ij3dviewer;
 
 import ij.ImagePlus;
@@ -29,7 +77,9 @@ import org.knime.knip.imagej2.core.util.ImgToIJ;
 
 import view4d.Timeline;
 import view4d.TimelineGUI;
-
+/* Helper class for the IJ3D viewer, provides the TableCellView
+ * @author <a href="mailto:gabriel.einsdorf@uni.kn">Gabriel Einsdorf</a>
+ */
 public class IJ3DTableCellView<T extends RealType<T>> implements TableCellView {
 
 	private NodeLogger logger = NodeLogger.getLogger(IJ3DTableCellView.class);
@@ -74,12 +124,9 @@ public class IJ3DTableCellView<T extends RealType<T>> implements TableCellView {
 
 		// Menubar
 		IJ3DMenubar<T> ij3dbar = new IJ3DMenubar<T>(universe, this);
-		// Image3DMenubar ij3dbar = new Image3DMenubar(universe);
 
 		rootPanel.add(ij3dbar, BorderLayout.NORTH);
 		rootPanel.add(universe.getCanvas(0), BorderLayout.CENTER);
-
-		// rootPanel.add(panel4D, BorderLayout.SOUTH);
 
 		return rootPanel;
 	}
@@ -96,16 +143,19 @@ public class IJ3DTableCellView<T extends RealType<T>> implements TableCellView {
 		universe.removeAllContents(); // cleanup universe
 
 		dataValue = valueToView;
-		// convert to IJ
+
 		@SuppressWarnings("unchecked")
 		ImgPlus<T> imgPlus = ((ImgPlusValue<T>) valueToView).getImgPlus();
 		
-		if(imgPlus.numDimensions() > 5){
+		//escapes 
+		if(imgPlus.numDimensions() > 5 ){
 			logger.warn("Error: only immages with up to 5 Dimensions are supported by the 3D viewer");
 			return;
 		}
+		
 		ImgToIJ imgToIJ = new ImgToIJ(imgPlus.numDimensions());
-
+		
+		//validate if mapping can be inferred automatically
 		if (!imgToIJ.validateMapping(imgPlus)) {
 			logger.warn("Warning: couldn't match dimensions of input picture, using Standard dimension settings as best guess");
 			HashMap<AxisType, Integer> newMapping = new HashMap<AxisType, Integer>();
@@ -116,9 +166,9 @@ public class IJ3DTableCellView<T extends RealType<T>> implements TableCellView {
 
 			imgToIJ.setMapping(newMapping);
 		}
-		
+		//convert to IJ
 		ijImagePlus = Operations.compute(imgToIJ, imgPlus);
-
+		
 		new StackConverter(ijImagePlus).convertToGray8();
 
 		switch (renderType) {
@@ -137,12 +187,13 @@ public class IJ3DTableCellView<T extends RealType<T>> implements TableCellView {
 		case ContentConstants.SURFACE_PLOT2D:
 			c = universe.addSurfacePlot(ijImagePlus);
 			break;
-		default: // TODO remove Evil Haxx :D
+		default:
 			throw new RuntimeException(
 					"no RenderMode selected, This should not have happend!!!");
 		}
 		universe.updateTimeline();
 
+		//enables the timeline gui if picture has 4 or 5 Dimensions
 		if (imgPlus.numDimensions() > 3) {
 			timelineGUI = new TimelineGUI(timeline);
 			panel4D = timelineGUI.getPanel();
@@ -156,7 +207,6 @@ public class IJ3DTableCellView<T extends RealType<T>> implements TableCellView {
 
 	@Override
 	public void onClose() {
-		// Called on close (Cleanup memory etc) TODO:?
 	}
 
 	@Override
@@ -171,18 +221,14 @@ public class IJ3DTableCellView<T extends RealType<T>> implements TableCellView {
 
 	@Override
 	public void loadConfigurationFrom(ConfigRO config) {
-		// Bene: Nothing to do here
 	}
 
 	@Override
 	public void saveConfigurationTo(ConfigWO config) {
-		// Bene: Nothing to do here
 	}
 
 	@Override
 	public void onReset() {
-		universe.removeAllContents(); // TODO needed?
-		// Called on reset of the TableCellViewer
 	}
 
 }
